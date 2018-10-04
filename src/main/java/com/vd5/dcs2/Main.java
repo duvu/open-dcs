99c9c3658c63540f4f41c181671ff3abbc99f213
beou <hoaivubk@gmail.com>
2018-10-05 05:56:26 +0700

    Heartbeat

--- src/main/java/com/vd5/dcs2/Main.java
+++ src/main/java/com/vd5/dcs2/Main.java
@@ -1,18 +1,29 @@
 package com.vd5.dcs2;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 /**
  * @author beou on 10/1/18 17:33
  */
 public class Main {
-
+    private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(2);
     public static void main(String[] args) {
         Log.info("Initiating DCS server ...");
         try {
-            ApplicationContext.getWebClient().send("###Test###");
             ApplicationContext.getServerManager().start();
         } catch (InterruptedException e) {
             e.printStackTrace();
             Log.error("ERROR", e);
         }
+        scheduler.scheduleAtFixedRate(new Runnable() {
+
+            @Override
+            public void run() {
+                ApplicationContext.getWebClient().send("###Test###");
+            }
+        }, 3, 10, TimeUnit.SECONDS);
     }
 }
