470bcba9602defe80787fddeb4ca57aa9f66141c
beou <hoaivubk@gmail.com>
2018-10-15 00:12:40 +0700

    init wlink protocol

--- src/main/java/com/vd5/dcs2/protocol/wlink/WlinkProtocolDecoder.java
+++ src/main/java/com/vd5/dcs2/protocol/wlink/WlinkProtocolDecoder.java
@@ -1,10 +1,11 @@
 package com.vd5.dcs2.protocol.wlink;
 
 import com.vd5.dcs2.AbstractProtocolDecoder;
-import com.vd5.dcs2.Log;
 import com.vd5.dcs2.Protocol;
 import com.vd5.dcs2.model.Position;
 import io.netty.channel.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.SocketAddress;
 
@@ -12,7 +13,7 @@ import java.net.SocketAddress;
  * @author beou on 10/14/18 23:31
  */
 public class WlinkProtocolDecoder extends AbstractProtocolDecoder {
-
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
     public WlinkProtocolDecoder(Protocol protocol) {
         super(protocol);
     }
@@ -20,7 +21,7 @@ public class WlinkProtocolDecoder extends AbstractProtocolDecoder {
     @Override
     protected Object decode(Channel channel, SocketAddress remoteAddress, Object msg) throws Exception {
         if (msg instanceof String) {
-            Log.info("Got data: " + msg);
+            log.info("Got data: " + msg);
         }
 
         return new Position();
