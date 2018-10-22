7a0be5a3793a82690e2ea40f2da8382630b46db0
beou <hoaivubk@gmail.com>
2018-10-22 17:56:38 +0700

    update wlink protocol

--- src/main/java/com/vd5/dcs2/protocol/wlink/WlinkPatterns.java
+++ src/main/java/com/vd5/dcs2/protocol/wlink/WlinkPatterns.java
@@ -152,7 +152,7 @@ public class WlinkPatterns {
             .expression("(?:RESP|BUFF):GTFRI,")
             .number("([0-9A-Z]{2}xxxx)?,")     // protocol version
             .number("(d{15}|x{14}),")            // imei
-            .expression("(?:([0-9A-Z]{17}),)?")  // vin
+            .expression("(([0-9A-Z]{17}),)?")  // vin
             .expression("([^,]*),")                // device name
             .number("(d+),")                    // power
             .number("d{1,2},")                   // report type
