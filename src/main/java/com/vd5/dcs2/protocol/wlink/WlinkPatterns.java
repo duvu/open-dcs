2432f179c9ae85b621aea8cd0b0a746aca14b7ae
beou <hoaivubk@gmail.com>
2018-10-22 17:44:58 +0700

    update wlink protocol

--- src/main/java/com/vd5/dcs2/protocol/wlink/WlinkPatterns.java
+++ src/main/java/com/vd5/dcs2/protocol/wlink/WlinkPatterns.java
@@ -153,7 +153,7 @@ public class WlinkPatterns {
             .number("([0-9A-Z]{2}xxxx)?,")     // protocol version
             .number("(d{15}|x{14}),")            // imei
             .expression("(?:([0-9A-Z]{17}),)?")  // vin
-            .expression("[^,]*,")                // device name
+            .expression("([^,]*),")                // device name
             .number("(d+),")                    // power
             .number("d{1,2},")                   // report type
             .number("d{1,2},")                   // count
